b6ea2eac835febdb966e027149f5fad506994635
Tyler-Applegate <tyler.applegate@gmail.com>
2021-04-13 17:02:27 -0500

    added commenting and alternative methodology

---
+++ import_exercises.py
@@ -0,0 +1,23 @@
+# Exercises
+# You will need to use imports to complete each exercise; in addition, these exercise will strengthen your problem solving and python coding skills.
+
+# You will be directed to create specific files in part 1, for the rest you may do your work in either import_exercises.py or import_exercises.ipynb.
+
+# 1. Import and test 3 of the functions from your functions exercise file. Import each function in a different way:
+
+# a. Run an interactive python session and import the module. Call the is_vowel function using the . syntax.
+
+# make sure to change directory to be in python-exercises
+# open an iPython session in the terminal, under the python-exercises folder
+
+# IMPORT function_exercises
+
+# function_exercises.is_vowel('a')
+
+# b. Create a file named import_exericses.py. Within this file, use from to import the calculate_tip function directly. 
+# Call this function with values you choose and print the result.
+
+FROM function_exercises IMPORT calculate_tip
+
+calculate_tip(100, .3)
+
